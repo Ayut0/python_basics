66a074d608fb3b7c4d82ecfb91ee42c04b0889ca
Yuto Yama <yuvto.0420@gmail.com>
2024-04-30 15:58:38 -0700

    add a specific error message and http error code

--- routes/main.py
+++ routes/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from typing import Union
 from pydantic import BaseModel
 
@@ -9,13 +9,23 @@ class Item(BaseModel):
     price: float
     is_offer: Union[bool, None] = None
 
+
+items = []
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int, q:Union[str, None] = None):
+#     return {"item_id":item_id, "q":q}
+
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q:Union[str, None] = None):
-    return {"item_id":item_id, "q":q}
+def get_item(item_id: int) -> str:
+    if item_id < len(items):
+        return items[item_id]
+    else:
+        raise HTTPException(status_code=404, detail="Items not found")
 
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
